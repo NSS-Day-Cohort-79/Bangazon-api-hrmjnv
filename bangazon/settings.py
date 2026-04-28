2ee99501773bd6303ec1c865a39c58497427ab1f
cmdrumright <cdrumright@protonmail.com>
2026-04-28 13:51:20 -0500

    Fixed current tests failing. - Added debug launch option for runing tests - Changed settings to avoid converting decimal to string - Fixed min/max validator setting on product price field

--- bangazon/settings.py
+++ bangazon/settings.py
@@ -54,6 +54,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'COERCE_DECIMAL_TO_STRING': False,
     'PAGE_SIZE': 10
 }
 
