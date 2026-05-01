3529cb69e5aaeb09eb31ceffd10e25c02157bced
Jack Gardner <jtgardner99@gmail.com>
2026-05-01 13:48:02 -0400

    safe delete parameter added to store model

--- bangazonapi/models/store.py
+++ bangazonapi/models/store.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from safedelete.models import SafeDeleteModel, SOFT_DELETE_CASCADE
 
 
-class Store(models.Model):
+class Store(SafeDeleteModel):
 
+    _safedelete_policy = SOFT_DELETE_CASCADE
     seller = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=55)
     description = models.CharField(max_length=255)
